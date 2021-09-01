0f9b3845add3fc1ae560a2454d0cdb681655eb5c
NiverMtz <nivermartinezmed@gmail.com>
2021-09-01 11:58:00 -0500

    First exercise in JS

---
+++ main.js
@@ -0,0 +1,12 @@
+// alert("Hola Mundo"); //Comentario en línea
+
+/* Comentario en bloque
+alert('Hola Mundo')
+*/
+
+var nombre = "marlen";
+var apellidos = "santamaria";
+var edad = 21;
+
+console.log(typeof nombre);
+console.log(typeof edad);
