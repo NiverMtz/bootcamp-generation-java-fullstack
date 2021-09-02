39538539ac986f7575ed490bc68783423912fd4e
NiverMtz <nivermartinezmed@gmail.com>
2021-09-02 13:29:13 -0500

    ejercicio 2

---
+++ ejercicio2/main.js
@@ -0,0 +1,46 @@
+// Validación de Edad
+
+// var edad = 13;
+
+// if (edad >= 18 && edad < 25) {
+//     console.log("Adulto Joven")
+// } else if (edad >= 25) {
+//     console.log("Mayor de edad")
+// } else {
+//     console.log("Menor de edad")
+// }
+
+// Función Suma
+
+// function add(a, b) {
+//     return a + b;
+// }
+
+// console.log(add(5,4))
+
+// Variables
+
+var nombre = "Niver";
+var apellidos = "Mtz";
+var edad = "25";
+
+var numero = 40;
+
+if (true) {
+  var numero = 50;
+  console.log(numero);
+}
+
+console.log(numero);
+
+
+var numero2 = 40;
+
+console.log(numero2);
+
+if (true) {
+  let numero2 = 5;
+  console.log(numero2);
+}
+
+console.log(numero2);
